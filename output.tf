1d4da66a8fd7442907c2fc5acd040227a1383c7e
rocky00000-netzero <130683351+rocky00000-netzero@users.noreply.github.com>
2025-04-15 17:10:27 +0900

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,11 +1,27 @@
 output "pet_example_one_id" {
-  value = module.example-module.random_pet_id_1
+  value = random_pet.example_one.id
 }
 
 output "pet_example_two_id" {
-  value = module.example-module.random_pet_id_2
+  value = random_pet.example_two.id
 }
 
 output "pet_example_three_id" {
-  value = module.example-module.random_pet_id_2
+  value = random_pet.example_three.id
+}
+
+output "pet_example_four_id" {
+  value = random_pet.example_four.id
+}
+
+output "pet_example_five_id" {
+  value = random_pet.example_five.id
+}
+
+output "pet_example_six_id" {
+  value = random_pet.example_six.id
+}
+
+output "pet_example_seven_id" {
+  value = random_pet.example_six.id
 }

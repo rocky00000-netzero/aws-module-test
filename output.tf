b87ccf9f303ab41af9de2ece3025aa3a03562466
rocky00000-netzero <130683351+rocky00000-netzero@users.noreply.github.com>
2025-04-15 16:50:42 +0900

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,11 @@
+output "pet_example_one_id" {
+  value = module.example-module.random_pet_id_1
+}
+
+output "pet_example_two_id" {
+  value = module.example-module.random_pet_id_2
+}
+
+output "pet_example_three_id" {
+  value = module.example-module.random_pet_id_2
+}
